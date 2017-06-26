4ea06629ff8fb4f065b4319bd9dde416ebcad62a
Maya Jeyendran <mayajey@fb.com>
2017-06-26 13:56:30 -0700

    user added

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -1,16 +1,7 @@
 package com.codepath.apps.restclienttemplate.models;
 
-import com.codepath.apps.restclienttemplate.MyDatabase;
-import com.raizlabs.android.dbflow.annotation.Column;
-import com.raizlabs.android.dbflow.annotation.PrimaryKey;
-import com.raizlabs.android.dbflow.annotation.Table;
-import com.raizlabs.android.dbflow.sql.language.Select;
-import com.raizlabs.android.dbflow.structure.BaseModel;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.List;
 
 /*
  * This is a temporary, sample model that demonstrates the basic structure
@@ -20,53 +11,21 @@ import java.util.List;
  * Note: All models **must extend from** `BaseModel` as shown below.
  * 
  */
-// parcelable
-@Table(database = MyDatabase.class)
-public class Tweet extends BaseModel {
+// parcelable here
+public class Tweet{
 
-	@PrimaryKey
-	@Column
-	Long id;
+	public String body; // tweet body maxChar 140
+	public String createdAt; // time created
+	public long uid; // database ID
+	// public User user; // info abt the user
 
-	// Define table fields
-	@Column
-	private String name;
-
-	public Tweet() {
-		super();
+	public static Tweet fromJSON(JSONObject jsonObject) throws JSONException {
+		Tweet tweet = new Tweet();
+		// populate tweet info
+		tweet.body = jsonObject.getString("text");
+		tweet.uid = jsonObject.getLong("id");
+		tweet.createdAt = jsonObject.getString("created_at");
+		return tweet;
 	}
 
-	// Parse model from JSON
-	public Tweet(JSONObject object){
-		super();
-
-		try {
-			this.name = object.getString("title");
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-	}
-
-	// Getters
-	public String getName() {
-		return name;
-	}
-
-	// Setters
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	/* The where class in this code below will be marked red until you first compile the project, since the code 
-	 * for the SampleModel_Table class is generated at compile-time.
-	 */
-	
-	// Record Finders
-	public static Tweet byId(long id) {
-		return new Select().from(Tweet.class).where(Tweet_Table.id.eq(id)).querySingle();
-	}
-
-	public static List<Tweet> recentItems() {
-		return new Select().from(Tweet.class).orderBy(Tweet_Table.id, false).limit(300).queryList();
-	}
 }
