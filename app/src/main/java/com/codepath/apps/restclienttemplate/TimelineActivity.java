d31d2a5a58aabfbc0c03db9e453f5bc54078f040
Maya Jeyendran <mayajey@fb.com>
2017-07-03 15:57:28 -0700

    intermittent profile view. now compose is broken :)

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -2,24 +2,37 @@ package com.codepath.apps.restclienttemplate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.codepath.apps.restclienttemplate.fragments.TweetsListFragment;
+import com.codepath.apps.restclienttemplate.fragments.HomeTimelineFragment;
+import com.codepath.apps.restclienttemplate.fragments.TweetsPagerAdapter;
 import com.codepath.apps.restclienttemplate.models.Tweet;
 
 public class TimelineActivity extends AppCompatActivity {
 
-    TweetsListFragment fragmentTweetsList;
     private final int COMPOSE_REQUEST_CODE = 10;
     MenuItem miActionProgressItem;
+    HomeTimelineFragment homeTimelineFragment;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
-        fragmentTweetsList = (TweetsListFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_timeline);
+        // init hometimelinefragment?
+
+        // get the view pager
+        ViewPager vpPager = (ViewPager) findViewById(R.id.viewpager);
+
+        // set adapter for pager
+        vpPager.setAdapter(new TweetsPagerAdapter(getSupportFragmentManager(), this));
+
+        // configure tab layout to use pager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(vpPager);
     }
 
     @Override
@@ -40,16 +53,15 @@ public class TimelineActivity extends AppCompatActivity {
         startActivityForResult(intent, COMPOSE_REQUEST_CODE);
     }
 
+    public void onProfileView(MenuItem item) {
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // check request code and result code first
         if (resultCode == requestCode) {
             Tweet newTweet = (Tweet) data.getParcelableExtra("tweet");
-            fragmentTweetsList.afterNewTweet(newTweet);
+            homeTimelineFragment.afterNewTweet(newTweet);
         }
     }
-
-
-
-
 }
--- app/src/main/java/com/codepath/apps/restclienttemplate/fragments/TweetsPagerAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/fragments/TweetsPagerAdapter.java
@@ -1,5 +1,8 @@
 package com.codepath.apps.restclienttemplate.fragments;
 
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 /**
@@ -7,4 +10,36 @@ import android.support.v4.app.FragmentPagerAdapter;
  */
 
 public class TweetsPagerAdapter extends FragmentPagerAdapter {
+
+    private String tabTitles[] = new String[] {"Home", "Mentions"};
+    private Context context;
+
+    public TweetsPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    // return total number of fragments there are
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    // return fragment to use depending on position
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new HomeTimelineFragment();
+        }
+        else if (position == 1) {
+            return new MentionsTimelineFragment();
+        }
+        else return null;
+    }
+
+    // return fragment title used for each tab
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabTitles[position];
+    }
 }
