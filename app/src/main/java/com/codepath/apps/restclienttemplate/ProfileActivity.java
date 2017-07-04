4acf95d3714fa65b52ca9dc66b5735c67d362802
Maya Jeyendran <mayajey@fb.com>
2017-07-03 17:21:46 -0700

    working on profile activity

--- app/src/main/java/com/codepath/apps/restclienttemplate/ProfileActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ProfileActivity.java
@@ -1,12 +1,22 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.TextView;
 
 import com.codepath.apps.restclienttemplate.fragments.UserTimelineFragment;
+import com.codepath.apps.restclienttemplate.models.User;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    TwitterClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,27 @@ public class ProfileActivity extends AppCompatActivity {
 
         ft.replace(R.id.flContainer, userTimelineFragment);
         ft.commit();
+
+        client = TwitterApp.getRestClient();
+        client.getUserInfo(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                User user = null;
+                try {
+                    user = User.fromJSON(response);
+                    getSupportActionBar().setTitle(user.screenName);
+                    populateUserHeadline(user);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+
+    public void populateUserHeadline(User user) {
+        TextView tvName = (TextView) findViewById(R.id.tvUserName);
+        TextView tvTagline = (TextView) findViewById(R.id.tvTagline);
+
     }
 }
